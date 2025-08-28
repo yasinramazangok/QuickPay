81e38b2f676843ad0e47a9e7fc7a81050d4438f8
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-08-28 23:32:41 +0300

    feat(application-layer): add payment request and response dto

---
+++ Backend/QuickPay/QuickPay.Application/DTOs/PaymentRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickPay.Application.DTOs
+{
+    public record PaymentRequestDto
+    (
+        string CardNumber,
+        string Expiry, // MM/YY or MM/YYYY
+        string Cvv,
+        decimal Amount,
+        string Currency = "USD"
+    );
+}
