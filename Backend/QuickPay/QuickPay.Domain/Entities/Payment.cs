d8f81999c08a2fe34afdde6677bdcec6b2f640d8
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-08-28 22:06:59 +0300

    chore(domain-layer): add Payment entity

---
+++ Backend/QuickPay/QuickPay.Domain/Entities/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickPay.Domain.Entities
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; } = "USD";
+        public string CardLast4 { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public string? Reason { get; set; }
+    }
+}
