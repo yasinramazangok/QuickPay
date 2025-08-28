a3260c8774385285485bfe34fe1e4dc6393e02af
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-08-29 01:39:56 +0300

    chore(api-layer): update Program.cs for service DI

--- Backend/QuickPay/QuickPay.API/Program.cs
+++ Backend/QuickPay/QuickPay.API/Program.cs
@@ -1,4 +1,6 @@
 
+using QuickPay.Application.Services;
+
 namespace QuickPay.API
 {
     public class Program
@@ -8,6 +10,8 @@ namespace QuickPay.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            // Application services
+            builder.Services.AddScoped<IPaymentService, PaymentService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
